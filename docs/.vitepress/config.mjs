39e0b50ddd5bc338be6b40f858b0ddb59369187e
ThinkPad T14 <1327118851@qq.com>
2024-08-26 12:33:27 +0800

    🐞 fix: 修复提交者贡献者映射错误

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -78,16 +78,17 @@ export default defineConfig({
       GitChangelog({
         // 填写在此处填写您的仓库链接
         repoURL: () => 'https://github.com/damokeris/damokeris.github.io',
+
         // 加入Git提交者 到 贡献者 的映射关系
-        mapAuthors: [ 
+        mapAuthors: [
           {
-            name: 'damokeris',
-            username: 'intel-i5-13400', 
-            mapByEmailAliases: ['1327118851@qq.com'] 
+            name: 'intel-i5-13400',
+            username: 'damokeris', 
+            mapByEmailAliases: '1327118851@qq.com'
           },
           {
-            name: 'damokeris',
-            username: 'ThinkPad T14', 
+            name: 'ThinkPad T14',
+            username: 'damokeris', 
             mapByEmailAliases: ['1327118851@qq.com'] 
           },
         ],
